de2944b83dc9d3228eac47b01e2187d3fc7f64ea
2024105070004 <2024105070004@aqs-lab-c-15.mshome.net>
2025-04-24 14:42:59 -0400

    Criada a classe temperatura e iniciada a implementação da classe TemperaturaEspecial

--- ConversorTemperatura/src/main/java/Temperatura.java
+++ ConversorTemperatura/src/main/java/Temperatura.java
@@ -1,12 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author 2024105070004
- */
 public class Temperatura {
+    //atributo privado para armazenar temp-celsius
+    private double tempCelsius;
     
+    //construtor - criar o obj com uma temp inicial
+    public Temperatura(double tempInicialCelsius){
+        this.tempCelsius = tempInicialCelsius;
+    }   
+        //getter - para ler a temperatura
+        public double getTemperaturaCelsius(){
+            return tempCelsius;
+        }
+        
+        //setter - p mudar a temp
+        public void setTemperaturaCelsius(double mudarTempCelsius){
+            this.tempCelsius = mudarTempCelsius;
+        }
+        
+        //metodo para converter celsius para fahrenheit
+        public double converteParaFahrenheit(){
+            return (tempCelsius*9/5)+32;
+        }
+        //metodo para converter celsius para kelvin
+        public double converterParaKelvin(){
+            return tempCelsius+273.15;
+        }
 }
